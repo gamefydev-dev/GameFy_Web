edce417aa9b596c66405abb74a0d85ccf69a07d4
gamefydev-dev <gamefydev@gmail.com>
2025-09-08 00:41:44 -0300

    Atualização do ES Lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASEPATH || ''
+  basePath: process.env.BASEPATH || '',
+  eslint: {
+    ignoreDuringBuilds: true // não falha o build por erros de ESLint
+  }
 }
 
 export default nextConfig
